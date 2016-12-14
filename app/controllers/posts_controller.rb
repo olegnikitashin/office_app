4a69b13119abf1a83f981af409cab7b51ec4a91a
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-12-14 18:53:43 +0300

    Add Post Create, Show Actions & Bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,35 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :delete]
+
   def index
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      flash[:success] = 'Post was successfully created.'
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,33 @@ describe 'navigate' do
       expect(page).to have_content(/Posts/)
     end
   end
+
+  describe 'creation' do
+    before do
+      user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Kieran', last_name: 'Hebden')
+      login_as(user, scope: :user)
+      visit new_post_path
+    end
+
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some rationale'
+      click_on 'Save'
+
+      expect(page).to have_content('Some rationale')
+    end
+
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'User Association'
+      click_on 'Save'
+      # byebug
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
 end
