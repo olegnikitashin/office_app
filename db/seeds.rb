1da79f98ecd5367eb3162ae6a487252eeb0b9669
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-01-02 21:07:25 +0300

    Update the audit log factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,20 @@
                      password_confirmation: 'asdfasdf',
                      first_name: 'Kieran',
                      last_name: 'Hebden',
-                     phone: '123123123')
+                     phone: '1231231231')
 AdminUser.create!(email: 'admin@test.com',
                   password: 'asdfasdf',
                   password_confirmation: 'asdfasdf',
                   first_name: 'Oleg',
                   last_name: 'Admin',
-                  phone: '123123123')
+                  phone: '1231231231')
 
 20.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+end
+
+20.times do |audit_log|
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts '20 posts have been created!'
