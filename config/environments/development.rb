9661cd46608454785b684122caadce45f7154404
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-01-04 19:52:19 +0300

    Add basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,6 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -53,4 +51,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'no-reply@example.com' }
 end
