117d20f7a8e3cf78bd90590fdd97372db491a585
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-01-06 00:43:35 +0300

    Add Employee model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@user = User.create!(email: 'test@test.com',
+@employee = Employee.create!(email: 'test@test.com',
                      password: 'asdfasdf',
                      password_confirmation: 'asdfasdf',
                      first_name: 'Kieran',
@@ -19,14 +19,14 @@ AdminUser.create!(email: 'admin@test.com',
                   last_name: 'Admin',
                   phone: '1231231231')
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts '3 audit logs have been created!'
 
 20.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts '20 posts have been created!'
